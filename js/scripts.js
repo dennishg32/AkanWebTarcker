1bcbdd7414fcc507e827ea810d43b6e8bdf4700c
Dennis HG <dennishg32@gmail.com>
2020-09-25 15:59:45 +0200

    variables to use to validate

---
+++ js/scripts.js
@@ -0,0 +1,6 @@
+//let's varidate inputs before
+
+var day = document.forms["form"]["day"];
+var month = document.forms["form"]["month"];
+var year = document.forms["form"]["year"];
+var gender = document.forms["form"]["gender"];
