743dffc393455365c67a3dd79f6e70807d84f4e3
Dennis HG <dennishg32@gmail.com>
2020-09-26 23:29:15 +0200

    checked formula and display message

--- js/scripts.js
+++ js/scripts.js
@@ -5,13 +5,6 @@ var month_error = document.getElementById("month_error");
 var year_error = document.getElementById("year_error");
 var gender_error = document.getElementById("gender_error");
 
-/*
-day.addEventListener("textInput", day_Verify);
-month.addEventListener("textInput", month_verify);
-year.addEventListener("textInput", year_verify);
-gender.addEventListener("textInput", gender_verify);
-*/
-
 function validate() {
     const form = document.getElementById("form");
     var day = parseInt(document.getElementById("day").value);
@@ -29,11 +22,18 @@ function validate() {
     if (day == 1 || month > 2) {
         if (day > monthDays[month - 1]) {
             console.log("Invalid date!");
+            /*
+                  day.style.border = "1px solid #red";
+                  day_error.style.display = "block";*/
             return false;
         }
     }
     if (month <= 0 || month > 12) {
         console.log("invalid month");
+        /*
+            month.style.border = "1px solid red";
+            month_error.style.display = "block";
+            */
         return false;
     }
     if (month == 2) {
@@ -51,6 +51,8 @@ function validate() {
         }
     }
 
+    //declaration of array of names to use
+
     arrMale = ["Kwasi", "Kwado", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
     arrFemale = ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"];
 
@@ -60,37 +62,16 @@ function validate() {
     var birthday =
         (cc / 4 - 2 * cc - 1 + (5 * year) / 4 + (26 * (month + 1)) / 10 + day) % 7;
 
-    //console.log(genderFemale);
+    document.getElementById("display").innerHTML = Math.round(birthday);
 
-    //trynna display error
-    /*
-
-    if (dayValue <= 0 && dayValue > 31) {
-        day.style.border = "1px solid red";
-        day_error.style.display = "block";
-        day.focus();
-        return false;
-    } else {}
-    if (monthValue <= 0 && monthValue > 12) {
-        month.style.border = "1px solid red";
-        month_error.style.display = "block";
-        month.focus();
-        return false;
-    } else {}
-
-    if (yearValue <= 0000 && yearValue > 9999) {
-        year.style.border = "1px solid red";
-        year_error.style.display = "block";
-        year.focus();
-        return false;
+    if (gender === "male") {
+        console.log(arrMale[Math.round(birthday)]);
+        document.getElementById("name").innerHTML = arrMale[Math.round(birthday)];
     }
-
-    if (genderMale.checked == true && genderFemale.checked == false) {
-        alert("You are Man");
-        //Akan name should include this
+    if (gender === "female") {
+        console.log(arrFemale[Math.round(birthday)]);
+        document.getElementById("name").innerHTML = arrFemale[Math.round(birthday)];
     } else {
-        alert("You are Woman");
+        console.log("verify your gender");
     }
-
-*/
 }
