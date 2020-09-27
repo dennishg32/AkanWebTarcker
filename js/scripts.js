d1be9c42934d65de3151d571c65d8f36b343deb3
Dennis HG <dennishg32@gmail.com>
2020-09-27 14:02:21 +0200

    finalization

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,4 @@
 //let's varidate inputs before
-
-var day_error = document.getElementById("day_error");
-var month_error = document.getElementById("month_error");
-var year_error = document.getElementById("year_error");
-var gender_error = document.getElementById("gender_error");
-
 function validate() {
     const form = document.getElementById("form");
     var day = parseInt(document.getElementById("day").value);
@@ -17,28 +11,24 @@ function validate() {
     console.log(year);
     console.log(gender);
 
-    // Create list of days of a month [assume there is no leap year by default]
+    // Create list of days of a month
     var monthDays = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
     if (day == 1 || month > 2) {
         if (day > monthDays[month - 1]) {
             console.log("Invalid date!");
+            alert("Invalid date");
             return false;
         }
     }
     if (month <= 0 || month > 12) {
         console.log("invalid month");
-        /*
-                month.style.border = "1px solid red";
-                month_error.style.display = "block";
-            */
+        alert("Please Enter month");
         return false;
     }
+
     if (day <= 0 || day > 31) {
         console.log("invalid day");
-        /*
-                           day.style.border = "1px solid #red";
-                           day_error.style.display = "block";
-                     */
+        alert("Please enter a day");
         return false;
     }
     if (month == 2) {
@@ -48,10 +38,12 @@ function validate() {
         }
         if (lyear == false && day >= 29) {
             console.log("Invalid date!");
+            alert("Invalid");
             return false;
         }
         if (lyear == true && day > 29) {
             console.log("Invalid date!");
+            alert("Invalid date");
             return false;
         }
     }
@@ -66,17 +58,19 @@ function validate() {
     var cc = (year - 1) / 100 + 1;
     var birthday =
         (cc / 4 - 2 * cc - 1 + (5 * year) / 4 + (26 * (month + 1)) / 10 + day) % 7;
+    document.getElementById("res").innerHTML = birthday.toFixed();
 
-    document.getElementById("display").innerHTML = Math.round(birthday);
-
-    if (gender === "male") {
-        console.log(arrMale[Math.round(birthday)]);
-        document.getElementById("name").innerHTML = arrMale[Math.round(birthday)];
-    }
-    if (gender === "female") {
-        console.log(arrFemale[Math.round(birthday)]);
-        document.getElementById("name").innerHTML = arrFemale[Math.round(birthday)];
+    if (gender === "") {
+        console.log("Cant be empty");
+        alert("Cant be empty");
     } else {
-        console.log("verify your gender");
+        if (gender === "male") {
+            console.log(arrMale[birthday.toFixed()]);
+            document.getElementById("name").innerHTML = arrMale[birthday.toFixed()];
+        }
+        if (gender === "female") {
+            console.log(arrFemale[birthday.toFixed()]);
+            document.getElementById("name").innerHTML = arrFemale[birthday.toFixed()];
+        }
     }
 }
